4fbc2d2d5c76e56486347bc040474660588d87c8
qwli7 <qwli7@iflytek.com>
2022-08-19 13:21:16 +0800

    first commit

--- src/main/java/com/liqiwen/media/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.liqiwen.media.const;
-
-public interface Constant {
-  String HTTP_PREFIX = "http://";
-  String HTTPS_PREFIX = "https://";
-}
--- src/main/java/com/liqiwen/media/exception/FileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.liqiwen.media.exception;
-
-public class FileNotFoundException extends RuntimeException {
- 
-  public FileNotFoundException(String msg) {
-    super(msg)
-  }
-  
-}
